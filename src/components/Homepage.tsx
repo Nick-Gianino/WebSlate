35b1ea887c86df931bbacfc05918dcc52d3b114d
Nick Johnson <nickjohn2323@gmail.com>
2023-09-16 22:43:21 -0400

    added images to cards

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,16 +4,21 @@ import '../styles.css';
 const LoggedInHomePage = () => {
     
     const boxes = [
-        { content: 'About Us', text: 'Discover WebSlate', subheader: 'Learn about WebSlate and why we should be your choice in creating your business online web presence', imageSrc: '/sample-01.png'},
-        { content: 'Services', text: 'Explore our range of services', subheader: 'Find the perfect solutions for your business online presence from graphic design to website development', imageSrc: '/sample-01.png' },
-        { content: 'Submit', text: 'Get in touch with us', subheader: 'Get in touch with us to receive a quote or ask any questions about our services', imageSrc: '/sample-01.png'},
+        { content: 'About Us', text: 'Discover WebSlate', subheader: 'Learn about WebSlate and why we should be your choice in creating your business online web presence', imageSrc: '/card_1.png'},
+        { content: 'Services', text: 'Explore our range of services', subheader: 'Find the perfect solutions for your business online presence from graphic design to website development', imageSrc: 'card_2.png' },
+        { content: 'Submit', text: 'Get in touch with us', subheader: 'Get in touch with us to receive a quote or ask any questions about our services', imageSrc: '/card_3.png' },
     ];
 
     return (
         <div className="container">
             {boxes.map((box, index) => (
                 <div className="box" key={index}>
-                    <img src={box.imageSrc} alt={`Image for Box ${index + 1}`} className="boximg" />
+                    <img
+                        src={box.imageSrc}
+                        alt={`Image for Box ${index + 1}`}
+                        className="boximg"
+                        style={index === 2 ? { width: '225px', height: '320px', } : index === 1 ? {paddingTop: '18px' } : {}}
+                    />
                     <div className="header">{box.content}</div>
                     <div className="subheader">{box.text}</div>
                     <p className="paragraph">{box.subheader}</p>
