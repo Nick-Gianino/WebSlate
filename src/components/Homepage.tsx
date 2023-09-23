61f2e9562cd4d5da6000fb9586c145c663bc7138
Nick Johnson <nickjohn2323@gmail.com>
2023-09-23 04:17:43 -0400

    some design work on boxes, fixed overflow issues with text at different window sizes and added css to ensure visibility at different window sizes

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -5,8 +5,8 @@ const LoggedInHomePage = () => {
     
     const boxes = [
         { content: 'About Us', text: 'Discover WebSlate', subheader: 'Learn about WebSlate and why we should be your choice in creating your business online web presence', imageSrc: '/card_1.png'},
-        { content: 'Services', text: 'Explore our range of services', subheader: 'Find the perfect solutions for your business online presence from graphic design to website development', imageSrc: 'card_2.png' },
-        { content: 'Submit', text: 'Get in touch with us', subheader: 'Get in touch with us to receive a quote or ask any questions about our services', imageSrc: '/card_3.png' },
+        { content: 'Services', text: 'Explore our range of services', subheader: 'Find the perfect solutions for your business online presence from graphic design to website development.', imageSrc: 'card_2.png' },
+        { content: 'Submit', text: 'Get in touch with us', subheader: 'Get in touch with us by sending us an email to receive a quote or ask any questions about the services we provide.', imageSrc: '/card_3.png' },
     ];
 
     return (
@@ -17,11 +17,11 @@ const LoggedInHomePage = () => {
                         src={box.imageSrc}
                         alt={`Image for Box ${index + 1}`}
                         className="boximg"
-                        style={index === 2 ? { width: '225px', height: '320px', } : index === 1 ? {paddingTop: '18px' } : {}}
+                        style={index === 2 ? { width: '225px', height: '310px', paddingLeft: '10px', paddingTop: '10px',} : index === 1 ? {paddingTop: '10px', paddingLeft: '' } : index === 0 ? {paddingLeft:'10px', paddingTop: ''} : {}} 
                     />
-                    <div className="header">{box.content}</div>
-                    <div className="subheader">{box.text}</div>
-                    <p className="paragraph">{box.subheader}</p>
+                    <div className="header" style={{ padding: '0 5px' }}>{box.content}</div>
+                    <div className="subheader" style={{ padding: '0 20px', marginBottom: '5px', paddingTop: "8px" }}>{box.text}</div>
+                    <p className="paragraph" style={{ padding: '0 10px', margin: '0', paddingTop: '15px' }}>{box.subheader}</p>
                 </div>
             ))}
         </div>
