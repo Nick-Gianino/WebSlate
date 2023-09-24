d4c7f28a609a8aa2175931501dafe00a5f3459d1
Nick Johnson <nickjohn2323@gmail.com>
2023-09-24 02:34:56 -0400

    .

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className='paragraphAU'>
             <p>About Us</p>
+            <p>- Webslate is a fully US based company that employs cutting edge tech to compete with major industry competetors for a much smaller cost to our clients. All work is personally done by highly educated industry professionals and specialists with your input and ideas to realize your business' goals and dreams online</p>
+            <p>- should probably fill out a parapgraph about us/owners and some small synopsis about how webslate came about yadda yadda</p>
         </div>
     );
 };
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -4,8 +4,8 @@ import '../styles.css';
 const LoggedInHomePage = () => {
     
     const boxes = [
-        { content: 'About Us', text: 'Discover WebSlate', subheader: 'Learn about WebSlate and why we should be your choice in creating your business online web presence', imageSrc: '/card_1.png'},
-        { content: 'Services', text: 'Explore our range of services', subheader: 'Find the perfect solutions for your business online presence from graphic design to website development.', imageSrc: 'card_2.png' },
+        { content: 'About Us', text: 'Discover WebSlate', subheader: 'Learn about WebSlate and why we should be your choice in creating your business\' online web presence', imageSrc: '/card_1.png'},
+        { content: 'Services', text: 'Explore our range of services', subheader: 'Find the perfect solutions for your business\' online presence from graphic design to website development.', imageSrc: 'card_2.png' },
         { content: 'Submit', text: 'Get in touch with us', subheader: 'Get in touch with us by sending us an email to receive a quote or ask any questions about the services we provide.', imageSrc: '/card_3.png' },
     ];
 
@@ -21,7 +21,7 @@ const LoggedInHomePage = () => {
                             style={index === 2 ? { width: '38%', height: 'auto', paddingTop: '30px', } : index === 1 ? {width: '55%', height: 'auto', paddingTop: '10px', paddingLeft: '' } : index === 0 ? {width: '53%', height: 'auto', paddingLeft: '10px', paddingTop: '' } : {}}
                         />
                     </div>
-                    <div className="header" style={{ padding: '0 5px', paddingTop:'20px', marginLeft: '25px' }}>{box.content}</div>
+                    <div className="header" style={{ padding: '0 5px', paddingTop:'20px', marginLeft: '10px' }}>{box.content}</div>
                     <div className="subheader" style={{ padding: '0 20px', marginBottom: '5px', paddingTop: "12px", marginLeft: '50px' }}>{box.text}</div>
                     <p className="paragraph" style={{ padding: '0 10px', margin: '5px', paddingTop: '5px' }}>{box.subheader}</p>
                 </div>
