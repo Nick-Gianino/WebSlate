43fe3883d7cf33ef86eaa69869b5a170b5c856fa
Nick Johnson <nickjohn2323@gmail.com>
2023-09-28 01:44:35 -0400

    filled in text for services page

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div>
+        <div className='paragraphSV'>
             <p>Services</p>
+            <p>Web Development and Design</p>
+            <p>WebSlate specializes in creating your business' online precence through graphic design of your logos, branding, company voice and the development of your business' website and online space.</p>
+            <p>bulletpoints: Responsive web design, conforming to both desktop, mobile and other sized screens. Seemless interactivity between devices. Clean and modern design and development. Design based on your client base to reach the customers you want to reach. 
+            </p>
+            <p>Webslate also provides hosting for you website so you can have everything you need in one place from trustworthy uptime, google ad-sense, analytics, and continued support from WebSlate </p>
         </div>
     );
 };
