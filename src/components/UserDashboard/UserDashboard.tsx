59b9cb93a4bcd3c05a5457eb76d95cb59b9f40b1
Nick Gianino <nickgianino4@gmail.com>
2023-09-30 23:11:50 -0500

    added modular login to dashboard

--- src/components/UserDashboard/UserDashboard.tsx
+++ src/components/UserDashboard/UserDashboard.tsx
@@ -1,11 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+
 const UserDashboard = () => {
+    const [user, setUser] = useState<User | null>(null);
+    const auth = getAuth();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setUser(user);
+        });
+        return () => unsubscribe();
+    }, [auth]);
+
     return (
         <div className="services-container">
-            <div className="service-section">
-                <div className="service-header">
-                    <h2>WHOAH</h2>
+            {user ? (
+                <div className="service-section">
+                    <div className="service-header">
+                        <h2>Welcome, {user.displayName}</h2>
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className="service-section">
+                    <div className="service-header">
+                        <h2>Please log in</h2>
+                    </div>
+                </div>
+            )}
             <div className="service-section">
                 <div className="service-content">
                     {/* Content goes here */}
