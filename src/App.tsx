b084eb25716d8b5c4441217aa80d03778a9d7583
Nick Gianino <nickgianino4@gmail.com>
2023-09-30 20:35:25 -0500

    changed app.tsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -13,63 +13,62 @@ import { User, getAuth, onAuthStateChanged } from 'firebase/auth';
 import 'firebase/auth';
 
 
-const App: React.FC = () => {
-  const [selectedComponent, setSelectedComponent] = useState<number>(0);
-  const [selectedButton, setSelectedButton] = useState<number>(0);
-  const [user, setUser] = useState<User | null>(null);
-
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  };
-
-
-  initializeApp(firebaseConfig);
-  // const analytics = getAnalytics(app);
-
-  const auth = getAuth();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, user => {
-      setUser(user);
-      if(user) {
-        setSelectedComponent(5);
-        setSelectedButton(5);
-      }
-    });
-
-    return () => unsubscribe();
-  }, [auth]);
-  const selectComponent = (index: number) => {
-    setSelectedComponent(index);
-    setSelectedButton(index);
-  };
-
-  return (
-    <div className="App">
-
-      <TopBar setSelectedComponent={setSelectedComponent} setSelectedButton={setSelectedButton} />
-
-      <div className = "Data">
-        {selectedComponent === 0 && (user ? <UserDashboard /> : <Homepage setSelectedComponent={setSelectedComponent} />)}
-        {selectedComponent === 1 && <AboutUs />}
-        {selectedComponent === 2 && <Services />}
-        {selectedComponent === 3 && <ContactUs />}
-        {selectedComponent === 4 && <UserDashboard />}
-        {selectedComponent === 5 && <UserDashboard />}
+const App = () => {
+    const [selectedComponent, setSelectedComponent] = useState<number>(0);
+    const [selectedButton, setSelectedButton] = useState<number>(0);
+    const [user, setUser] = useState<User | null>(null);
+  
+    const firebaseConfig = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+    };
+  
+    initializeApp(firebaseConfig);
+    // const analytics = getAnalytics(app);
+  
+    const auth = getAuth();
+  
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, user => {
+        setUser(user);
+        if(user) {
+          setSelectedComponent(5);
+          setSelectedButton(5);
+        }
+      });
+  
+      return () => unsubscribe();
+    }, [auth]);
+    const selectComponent = (index: number) => {
+      setSelectedComponent(index);
+      setSelectedButton(index);
+    };
+  
+    return (
+      <div className="App">
+  
+        <TopBar setSelectedComponent={setSelectedComponent} setSelectedButton={setSelectedButton} />
+  
+        <div className = "Data">
+          {selectedComponent === 0 && (user ? <UserDashboard /> : <Homepage setSelectedComponent={setSelectedComponent} />)}
+          {selectedComponent === 1 && <AboutUs />}
+          {selectedComponent === 2 && <Services />}
+          {selectedComponent === 3 && <ContactUs />}
+          {selectedComponent === 4 && <UserDashboard />}
+          {selectedComponent === 5 && <UserDashboard />}
+        </div>
+  
+        <div className = "BottomBar">
+        <BottomBar selectComponent={selectComponent} selectedButton={selectedButton} />
+        </div>
+  
       </div>
-
-      <div className = "BottomBar">
-      <BottomBar selectComponent={selectComponent} selectedButton={selectedButton} />
-      </div>
-
-    </div>
-  );
+    );
 };
 
 export default App;
