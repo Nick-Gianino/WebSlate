ce5020bec0c85fcc58d357ce86411607bd29e678
Nick Gianino <nickgianino4@gmail.com>
2023-09-30 23:42:27 -0500

    half created contact us submit

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -1,51 +1,66 @@
-import React, { FormEvent, useState } from 'react';
+import React, { useState, FormEvent } from 'react';
+import '../../styles.css'; 
 
 const ContactUs = () => {
-    const [name, setName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [message, setMessage] = useState<string>('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        // Handle form submission here
-        console.log(name, email, message);
-    };
-    return (
-        <div className="services-container">
-        <div className="service-section">
-            <div className="service-header">
-                <h2>Contact Us</h2>
-            </div>
-        </div>
-        <div className="service-section">
-            <div className="service-content">
-                <form onSubmit={handleSubmit}>
-                    <input 
-                        type="text" 
-                        placeholder="Your name" 
-                        value={name} 
-                        onChange={(e) => setName(e.target.value)} 
-                        required 
-                    />
-                    <input 
-                        type="email" 
-                        placeholder="Email" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)} 
-                        required 
-                    />
-                    <textarea 
-                        placeholder="Your message" 
-                        value={message} 
-                        onChange={(e) => setMessage(e.target.value)} 
-                        required 
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        </div>
+  const submitForm = (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(name, email, message);
+
+    saveContactInfo(name, email, message);
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  const saveContactInfo = (name: string, email: string, message: string) => {
+    // Save info to Firebase or other database here
+    console.log("aye we tried man");
+  };
+
+  return (
+    <>
+    <div>
+      <h1>Contact us</h1>
+      <p>Send a message and we will get back to you within 24 hours.</p>
+      <div className='services-container'>
+      <form className="contact-form" onSubmit={submitForm}>
+        <label>Name</label><br />
+        <input 
+          className="name" 
+          type="text" 
+          placeholder="name" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        /><br />
+        <label>Email</label><br />
+        <input 
+          className="email" 
+          type="email" 
+          placeholder="email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br />
+        <label>Message</label><br />
+        <textarea
+          className="message"
+          cols={30}
+          rows={10}
+          placeholder="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea><br />
+        <button type="submit">Submit</button>
+      </form>
+      </div>
     </div>
-    );
+    </>
+  );
 };
 
 export default ContactUs;
