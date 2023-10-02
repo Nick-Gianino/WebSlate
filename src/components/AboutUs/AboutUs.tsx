3d6221e45312d8dd6e3fac2e6ae40bf15f950044
Nick Johnson <nickjohn2323@gmail.com>
2023-10-02 17:59:19 -0400

    added styling to about us page, changed about us div names and placement to help with styling and worked on some of the contact us page styling.

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -3,14 +3,14 @@ import '../../styles.css'
 
 const AboutUs = () => {
     return (
-        <div className="services-container">
-            <div className="service-section">
-                <div className="service-header">
+        <div className="aboutus-container">
+            <div className="aboutus-section">
+                <div className="aboutus-header">
                     <h2>About Us</h2>
                 </div>
-            </div>
-            <div className="service-section">
-                <div className="service-content">
+            
+            
+                <div className="aboutus-content">
                     <p>- Webslate is a fully US based company that employs cutting edge tech to compete with major industry competitors for a much smaller cost to our clients. All work is personally done by highly educated industry professionals and specialists with your input and ideas to realize your business' goals and dreams online</p>
                     <p>- should probably fill out a paragraph about us/owners and some small synopsis about how webslate came about yadda yadda</p>
                 </div>
